9c2ee10a3e7509cd67f5604d4e3f698237a6721b
Demjan <demjan.vester@gmail.com>
2021-04-05 21:52:35 +0200

    explicit resource values

--- app/Http/Resources/BallotComponent.php
+++ app/Http/Resources/BallotComponent.php
@@ -14,6 +14,13 @@ class BallotComponent extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            "id" => $this->id,
+            "title" => $this->title,
+            "description" => $this->description,
+            "type" => $this->type,
+            "version" => $this->version,
+            "options" => $this->options
+        ];
     }
 }
