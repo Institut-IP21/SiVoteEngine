6b2adb28031c674ccda88a0870368e03c4ccf940
Demjan <demjan.vester@gmail.com>
2021-04-10 19:02:12 +0200

    names overwrote

--- app/BallotComponents/FirstPassThePost/v1/form.blade.php
+++ app/BallotComponents/FirstPassThePost/v1/form.blade.php
@@ -2,8 +2,8 @@
     <label class="radio flex -ml-2 p-2 mb-2 cursor-pointer hover:bg-blue-100 hover:bg-opacity-25"
         for="{{ $component->id }}--{{ $loop->iteration }}">
         <span class="radio__input">
-            <input type="radio" name="radio" id="{{ $component->id }}--{{ $loop->iteration }}"
-                name="{{ $component->id }}" value="{{ $option }}" />
+            <input type="radio" id="{{ $component->id }}--{{ $loop->iteration }}" name="{{ $component->id }}"
+                value="{{ $option }}" />
             <span class="radio__control mr-3"></span>
         </span>
         <span class="radio__label">{{ $option }}</span>
@@ -13,8 +13,7 @@
     <label class="radio flex -ml-2 p-2 mb-2 cursor-pointer hover:bg-blue-100 hover:bg-opacity-25"
         for="{{ $component->id }}--abstain">
         <span class="radio__input">
-            <input type="radio" name="radio" id="{{ $component->id }}--abstain" name="{{ $component->id }}"
-                value="abstain" />
+            <input type="radio" id="{{ $component->id }}--abstain" name="{{ $component->id }}" value="abstain" />
             <span class="radio__control mr-3"></span>
         </span>
         <span class="radio__label">Abstain</span>
