4d76107a72eba18b4fb9f92fb0061e39b3f89e18
Demjan <demjan.vester@gmail.com>
2021-08-14 11:31:53 +0200

    formatting

--- app/BallotComponents/RankedChoice/v1/result.blade.php
+++ app/BallotComponents/RankedChoice/v1/result.blade.php
@@ -1,11 +1,13 @@
 <div class="something" x-data="{ show_elimination: false }">
     @if ($results[$component->id]['results']['result']['conclussive'])
-        <button class="w-full text-center bg-green-200 hover:bg-green-400 p-3" x-on:click="show_elimination = !show_elimination">
+        <button class="w-full text-center bg-green-200 hover:bg-green-400 p-3"
+            x-on:click="show_elimination = !show_elimination">
             {{ __('components.rankedchoice.winner_is') }}
             {{ $results[$component->id]['results']['result']['conclussive_winner'] }}
         </button>
     @else
-        <button class="w-full text-center bg-yellow-200 hover:bg-yellow-400 p-3" x-on:click="show_elimination = !show_elimination">
+        <button class="w-full text-center bg-yellow-200 hover:bg-yellow-400 p-3"
+            x-on:click="show_elimination = !show_elimination">
             {{ __('components.rankedchoice.no_winner') }}
             {{ implode(', ', $results[$component->id]['results']['result']['winners']) }}.
         </button>
