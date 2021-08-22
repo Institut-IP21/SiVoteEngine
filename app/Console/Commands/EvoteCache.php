2d128ae45c29c44b9dfe7713e53d57e5198a26ef
Demjan <demjan.vester@gmail.com>
2021-08-22 12:00:02 +0200

    add full cache  command

---
+++ app/Console/Commands/EvoteCache.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class EvoteCache extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'evote:cache';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Caches everything it\'s supposed to';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $this->call('optimize:clear');
+
+        $this->call('route:scan');
+        $this->call('model:scan');
+
+        $this->call('config:cache');
+        $this->call('route:cache');
+        $this->call('view:cache');
+
+        $this->info('Cache cleared and created successfully!');
+    }
+}
