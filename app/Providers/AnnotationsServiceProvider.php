3e6b71ba8acd31a8e7abe244ec57ace145259fac
Demjan <demjan.vester@gmail.com>
2021-12-22 11:51:32 +0100

    perf improvement with annotations

--- app/Providers/AnnotationsServiceProvider.php
+++ app/Providers/AnnotationsServiceProvider.php
@@ -33,7 +33,7 @@ class AnnotationsServiceProvider extends ServiceProvider
      *
      * @var bool
      */
-    protected $scanWhenLocal = true;
+    protected $scanWhenLocal = false;
 
     /**
      * Determines whether or not to automatically scan the controllers
